63ba3ed3a11e2511336709aa3ff005622d6c45b1
msuarez <manuel.suarez@ednon.es>
2020-06-10 14:50:51 +0200

    Checking if db is up

--- image/entrypoint/initdb.sh
+++ image/entrypoint/initdb.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+until ping -c 1 -W 1 ${DATABASE_HOST:?missing environment variable. DATABASE_HOST must be set}
+    do
+        echo "Waiting for postgresql to ping..."
+        sleep 1s
+done
+
 python ./manage.py migrate
 python ./manage.py runserver 0.0.0.0:8000
